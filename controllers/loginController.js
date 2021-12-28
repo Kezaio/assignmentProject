7f6471d55aa4b790538baafbe65a5986fae1d5d3
likeke <2422014722@qq.com>
2021-12-28 15:34:50 +0800

    新增登陆注册时的错误提示

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -2,16 +2,18 @@ var User = require('../models/user');
 
 var async = require('async');
 
-// Display Author create form on GET
-exports.register_get = function (req, res, next) {
-  res.render('register', { title: 'register' });
-};
-
 // 注册
+exports.register_get = function (req, res, next) {
+  res.render('register', { title: 'register',passwordWrong:'' });
+};
 exports.register_post = function (req, res, next) {
   req.checkBody('username', 'username must be specified.').notEmpty(); //We won't force Alphanumeric, because people might have spaces.
   req.checkBody('password', 'password must be specified.').notEmpty();
-
+  req.sanitize('username').escape();
+  req.sanitize('password').escape();
+  req.sanitize('username').trim();
+  req.sanitize('password').trim();
+  
   var errors = req.validationErrors();
   var user = new User ({
     username: req.body.username,
@@ -19,32 +21,34 @@ exports.register_post = function (req, res, next) {
   });
 console.log('user',user);
   if (errors) {
-    res.render('register', { title: 'register',  errors: errors });
+    res.render('register', { title: 'register',  errors: errors,passwordWrong:'' });
     return;
   }
   else {
-    user.save(function (err) {
-      if (err) return next(err);
-      res.redirect('/login');
-    });
+    User.findOne({username: req.body.username}).exec(function (err, person) {
+      console.log('person',person);
+      if(err){
+        console.log(err);
+      }else{
+        if(person===null){
+          user.save(function (err) {
+            if (err) return next(err);
+            res.redirect('/login');
+            res.render('login', { title: 'login', passwordWrong:"注册成功，请重新登录！" });
+          });
+        }else{
+          res.render('register', { title: 'register', passwordWrong:"用户名已存在！",username:req.body.username,password:req.body.password });
+        }
+      }
+    })
   }
 };
 
-
-
-// Handle Author create on POST
-exports.login_yesorno = function (req, res, next) {
-  res.render('login', { title: 'Create Author' });
-};
-
-
-// Display Author create form on GET
-exports.login_get = function (req, res, next) {
-  console.log('login_get');
-  res.render('login', { title: 'Create Author' });
-};
-
 // 登录
+exports.login_get = function (req, res, next) {
+  res.render('login', { title: 'login',passwordWrong:'' });
+};
+
 exports.login_post = function (req, res, next) {
   req.checkBody('username', 'username must be specified.').notEmpty(); //We won't force Alphanumeric, because people might have spaces.
   req.checkBody('password', 'password must be specified.').notEmpty();
@@ -56,23 +60,34 @@ exports.login_post = function (req, res, next) {
   });
 console.log('user',user);
   if (errors) {
-    res.render('login', { title: 'login',  errors: errors });
+    res.render('login', { title: 'login',  errors: errors,passwordWrong:'' });
     return;
   }
   else {
     var query = User.findOne({
-      username: req.body.username,
-      password: req.body.password
+      username: req.body.username
     });
-query.select();
-query.exec(function (err, person) {
-    if (err) {
-        console.log('select failed');
-    } else {
-        console.log(person);
-        res.redirect('/catalog');
-    }
-});
+    query.select();
+    query.exec(function (err, person) {
+      console.log('err',err);
+      console.log('person',person);
+        if (err) {
+            console.log(err);
+        } else {
+          //判断是否注册
+          if(person!==null){
+            console.log(person._doc.password);
+            //判断密码是否正确
+            if(person._doc.password===req.body.password){
+              res.redirect('/catalog');
+            }else{
+              res.render('login', { title: 'login', passwordWrong:"密码错误！",username:req.body.username,password:req.body.password });
+            }
+          }else{
+            res.render('login', { title: 'login', passwordWrong:"未注册或用户名错误！",username:req.body.username,password:req.body.password });
+          }
+        }
+    });
   }
 };
 
